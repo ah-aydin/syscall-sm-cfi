c76715f83bb6c0ae6fb2f231f5e7fcda25e51e61
Ahmed Hamza Aydin <ahamzaaydin.py@gmail.com>
2023-04-10 17:08:28 +0200

    Take into account different threads and instances of a binary

--- syscall-sm-cfi-e-bpf-common/src/lib.rs
+++ syscall-sm-cfi-e-bpf-common/src/lib.rs
@@ -29,7 +29,6 @@ pub fn build_transition(bin_name: &str, from: u16, to: u16) -> [u8; 20] {
     let from_bytes = from.to_be_bytes();
     let to_bytes = to.to_be_bytes();
 
-    //let bin_len = bin_bytes.len();
     let bin_len = 16;
     let from_len = from_bytes.len();
     let to_len = to_bytes.len();
@@ -39,6 +38,26 @@ pub fn build_transition(bin_name: &str, from: u16, to: u16) -> [u8; 20] {
     result[..bin_len].copy_from_slice(&bin_bytes);
     result[bin_len..(bin_len + from_len)].copy_from_slice(&from_bytes);
     result[(bin_len + from_len)..(bin_len + from_len + to_len)].copy_from_slice(&to_bytes);
+
+    result
+}
+
+pub fn build_bin_pid_tgid(bin_name: &str, pid: u32, tgid: u32) -> [u8; 24] {
+    let mut result = [0; 24];
+
+    let bin_bytes = str_to_16(bin_name);
+    let from_bytes = pid.to_be_bytes();
+    let to_bytes = tgid.to_be_bytes();
+
+    let bin_len = 16;
+    let pid_len = from_bytes.len();
+    let tgid_len = to_bytes.len();
+    if bin_len + pid_len + tgid_len > 24 {
+        panic!("String is too long");
+    }
+    result[..bin_len].copy_from_slice(&bin_bytes);
+    result[bin_len..(bin_len + pid_len)].copy_from_slice(&from_bytes);
+    result[(bin_len + pid_len)..(bin_len + pid_len + tgid_len)].copy_from_slice(&to_bytes);
 
     result
 }
--- syscall-sm-cfi-e-bpf-ebpf/src/main.rs
+++ syscall-sm-cfi-e-bpf-ebpf/src/main.rs
@@ -10,13 +10,17 @@ use aya_bpf::{
     cty::c_long,
     helpers::{
         bpf_get_current_comm,
-        bpf_send_signal_thread
+        bpf_send_signal_thread,
+        bpf_get_current_pid_tgid
     },
     maps::HashMap,
 };
 use aya_log_ebpf::info;
 use core::str::from_utf8_unchecked;
-use syscall_sm_cfi_e_bpf_common::build_transition;
+use syscall_sm_cfi_e_bpf_common::{
+    build_transition,
+    build_bin_pid_tgid
+};
 
 const SYSCALL_NR_OFFSET: usize = 8;
 const SIGTERM: u32 = 15;
@@ -28,7 +32,7 @@ static mut SYS_SM_TRACKED_BINARIES: HashMap<[u8; 16], [u8; 1]> = HashMap::<[u8; 
 static mut SYS_SM_TRANSITIONS: HashMap<[u8; 20], [u8; 1]> = HashMap::<[u8; 20], [u8; 1]>::with_max_entries(1024, 0);
 
 #[map(name = "SYS_SM_LAST_SYSCALL")]
-static mut SYS_SM_LAST_SYSCALL: HashMap<[u8; 16], u16> = HashMap::<[u8; 16], u16>::with_max_entries(10, 0);
+static mut SYS_SM_LAST_SYSCALL: HashMap<[u8; 24], u16> = HashMap::<[u8; 24], u16>::with_max_entries(10, 0);
 
 #[tracepoint(name = "tracepoint_program")]
 pub fn tracepoint_program(ctx: TracePointContext) -> c_long {
@@ -39,7 +43,6 @@ pub fn tracepoint_program(ctx: TracePointContext) -> c_long {
 }
 
 fn try_tracepoint_program(ctx: TracePointContext) -> Result<c_long, c_long> {
-    let syscall_nr: u32 = unsafe { ctx.read_at(SYSCALL_NR_OFFSET)? };
     let bin_bytes: [u8; 16] = bpf_get_current_comm()?;
     let bin_name = unsafe { from_utf8_unchecked(&bin_bytes) };
 
@@ -48,10 +51,17 @@ fn try_tracepoint_program(ctx: TracePointContext) -> Result<c_long, c_long> {
         return Ok(0);
     }
 
+    let syscall_nr: u32 = unsafe { ctx.read_at(SYSCALL_NR_OFFSET)? };
+
+    // Build the key for binary with it's pid and tgid
+    let tgid = (bpf_get_current_pid_tgid() >> 32) as u32;
+    let pid = bpf_get_current_pid_tgid() as u32;
+    let bin_tgid_pid = build_bin_pid_tgid(bin_name, pid, tgid);
+
     // Check if first syscall
-    let prev_syscall_result = unsafe { SYS_SM_LAST_SYSCALL.get(&bin_bytes) };
+    let prev_syscall_result = unsafe { SYS_SM_LAST_SYSCALL.get(&bin_tgid_pid) };
     if prev_syscall_result.is_none() {
-        unsafe { SYS_SM_LAST_SYSCALL.insert(&bin_bytes, &(syscall_nr as u16), 0).unwrap() };
+        unsafe { SYS_SM_LAST_SYSCALL.insert(&bin_tgid_pid, &(syscall_nr as u16), 0).unwrap() };
         return Ok(0);
     }
 
@@ -60,11 +70,19 @@ fn try_tracepoint_program(ctx: TracePointContext) -> Result<c_long, c_long> {
     let syscall_nr_u16: u16 = syscall_nr as u16;
     let transition = build_transition(bin_name, prev_syscall_nr, syscall_nr_u16);
     if unsafe { SYS_SM_TRANSITIONS.get(&transition).is_some() } {
-        unsafe { SYS_SM_LAST_SYSCALL.insert(&bin_bytes, &syscall_nr_u16, 0).unwrap() };
+        unsafe { SYS_SM_LAST_SYSCALL.insert(&bin_tgid_pid, &syscall_nr_u16, 0).unwrap() };
         return Ok(0);
     }
 
-    info!(&ctx, "TERMINATED: illegal transition by {} | from {} to {}", bin_name, prev_syscall_nr, syscall_nr);
+    info!(
+        &ctx,
+        "TERMINATED: illegal transition by {} | pid: {} | tgid: {} | from {} to {}",
+        bin_name,
+        pid,
+        tgid,
+        prev_syscall_nr,
+        syscall_nr
+    );
     unsafe { bpf_send_signal_thread(SIGTERM) };
     // TODO maybe consider not terminating, but deniying access to the syscall
     // Ok(-1)
